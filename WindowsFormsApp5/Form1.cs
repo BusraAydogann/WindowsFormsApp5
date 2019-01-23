9650f08bdf4300027eed7a2c4e1a1fc189e111f2
720694-Ogle <720694-Ogle@WISSEN-PC>
2019-01-23 17:06:23 +0200

    merhabamesajkutusuatıldı

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp5
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba");
+        }
     }
 }
